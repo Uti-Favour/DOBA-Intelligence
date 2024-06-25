15ab80a1876fbf3d9ad8d565ed83ef2bdf578ebe
Uti-Favour <utifavour15@gmail.com>
2024-06-25 10:38:16 +0100

    Removed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
       <Services/>
       <Commitments/>
