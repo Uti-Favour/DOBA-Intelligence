8660440ff4040bd44d9df97ef75a9d712ec10d3e
Uti-Favour <utifavour15@gmail.com>
2024-07-01 08:52:06 +0100

    Stars @ testimonial

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,13 +8,20 @@ const Testimonials = () => {
        
           {testimonials.map((testimonial) => (
             <div className="bg-neutral-900 p-5 rounded-3xl space-y-8">
+              <div className="text-blue-400 space-x-2">
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              </div>
               <p className="text-gray-200">"{testimonial.description}"</p>
               <div className="flex items-center space-x-4">
                 <img src={testimonial.image} alt="testimonial-image" className="w-[50px] h-[50px] rounded-full"/>
               
               <div>
               <span className="font-semibold">{testimonial.name}</span> <br />
-              <span>@ {testimonial.company}</span>
+              <span className="text-gray-500">@ {testimonial.company}</span>
                 </div> 
                
               </div>
