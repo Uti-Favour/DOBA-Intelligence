f7c752b8a5c4c33a58112ca6c885e80a34997262
Uti-Favour <utifavour15@gmail.com>
2024-07-06 11:08:25 +0100

    404

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -8,11 +8,11 @@ const ErrorPage = () => {
     <div>
         <div className="bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] text-white ">
         <Navbar/>
-        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <main className="grid min-h-full place-items-center px-6 py-32 sm:py-40 lg:px-8">
         <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+          <p className="text-base font-semibold text-blue-400">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
+          <p className="mt-6 text-base leading-7 text-white">Sorry, we couldn’t find the page you’re looking for.</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
               to="/"
@@ -20,7 +20,7 @@ const ErrorPage = () => {
             >
               Go back home
             </Link>
-            <Link to="/contact" className="text-sm font-semibold text-gray-900">
+            <Link to="/contact" className="text-sm font-semibold text-white">
               Contact support <span aria-hidden="true">&rarr;</span>
             </Link>
           </div>
